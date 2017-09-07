f386221b78f933d99f7b629873fdfd5bbe7972b7
Peter Wang <happypeter1983@gmail.com>
2017-09-07 15:23:38 +0800

    layout

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Home from '../Home/Home'
+// import Cart from '../Cart/Cart'
+// import Order from '../Order/Order'
+
+class Main extends Component {
+  render () {
+    return (
+      <div className='main'>
+        <Home />
+      </div>
+    )
+  }
+}
+
+export default Main
